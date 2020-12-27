c07dc37f2d14cbe26efd51c5bc33003e7f120016
siddhantk232 <siddhantk232@gmail.com>
2020-12-27 12:18:27 +0000

    fish configs and aliases

---
+++ .config/fish/config.fish
@@ -0,0 +1,17 @@
+set fish_greeting
+
+set EDITOR nvim
+set NVM_DIR ~/.nvm
+set -x GOPATH ~/projects/go
+set -x PATH $PATH $GOPATH/bin
+set -x PATH $PATH ~/.config/scripts
+set -x PATH $PATH ~/.config/scripts
+
+
+alias cfish "nvim ~/.config/fish/config.fish"
+alias ci3 "nvim ~/.config/i3/config"
+alias ci3status "nvim ~/.config/i3/status.toml"
+alias cvim "nvim ~/.config/nvim/init.vim"
+alias config "/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+
+fish_vi_key_bindings
