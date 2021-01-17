5a4dcaf97860171762aff0764ff8a7c8c6743aa1
siddhantk232 <siddhantk232@gmail.com>
2021-01-17 19:20:53 +0530

    remap capslock to escape
    fish.config comments

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,7 @@
+# clears fish welcome message
 set fish_greeting
+
+# environment variables 
 
 set -x EDITOR nvim
 set -x NVM_DIR ~/.nvm
@@ -6,6 +9,7 @@ set -x GOPATH ~/projects/go
 set -x PATH $PATH $GOPATH/bin
 set -x PATH $PATH ~/.config/scripts
 
+# confif aliases (all lowercase and starts with c)
 
 alias cfish "nvim ~/.config/fish/config.fish"
 alias ci3 "nvim ~/.config/i3/config"
@@ -14,8 +18,17 @@ alias ci3status "nvim ~/.config/i3/status.toml"
 alias cvim "nvim ~/.config/nvim/init.vim"
 alias ctmux "nvim ~/.tmux.conf"
 
+# aliases for commands
+
 alias config "/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias todo "nvim ~/todo"
 alias e "nvim"
 
+# change caps lock key to escape 
+setxkbmap -option ctrl:nocaps
+
+# set caps lock as ctrl when pressed with other keys
+xcape -e 'Control_L=Escape'
+
+# enable vi
 fish_vi_key_bindings
