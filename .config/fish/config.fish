38eb355fa4974664ae16889d76d20ef8d76b8bd5
siddhantk232 <siddhantk232@gmail.com>
2021-01-26 14:15:12 +0530

    typos :|

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -11,7 +11,7 @@ set -x GOPATH ~/projects/go
 set -x PATH $PATH $GOPATH/bin
 set -x PATH $PATH ~/.config/scripts
 
-# confif aliases (all lowercase and starts with c)
+# config aliases (all lowercase and starts with c)
 
 alias cfish "nvim ~/.config/fish/config.fish"
 alias ci3 "nvim ~/.config/i3/config"
@@ -26,12 +26,6 @@ alias csessions "nvim ~/.config/scripts/sessions"
 alias config "/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
 alias todo "nvim ~/todo"
 alias e "nvim"
-
-# change caps lock key to escape 
-# setxkbmap -option ctrl:nocaps
-
-# set caps lock as ctrl when pressed with other keys
-# xcape -e 'Control_L=Escape'
 
 # enable vi
 fish_vi_key_bindings
