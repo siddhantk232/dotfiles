80beb6802ec3451438db91e8cb4e6e2f34f21dc5
siddhantCodes <siddhantk232@gmail.com>
2022-05-14 16:42:41 +0530

    nvim: stylua

--- nvim/.config/nvim/lua/siddhant/lsp/cmp.lua
+++ nvim/.config/nvim/lua/siddhant/lsp/cmp.lua
@@ -1,41 +1,39 @@
-local cmp = require 'cmp'
-local luasnip = require("luasnip")
+local cmp = require "cmp"
+local luasnip = require "luasnip"
 
 local has_words_before = function()
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and
-             vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col)
-                 :match("%s") == nil
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match "%s" == nil
 end
 
-vim.g.completion_matching_strategy_list = {'exact', 'substring', 'fuzzy'}
+vim.g.completion_matching_strategy_list = { "exact", "substring", "fuzzy" }
 
-cmp.setup({
+cmp.setup {
   snippet = {
     expand = function(args)
       luasnip.lsp_expand(args.body) -- For `luasnip` users.
-    end
+    end,
   },
 
   view = {
-    entries = 'native'
+    entries = "native",
   },
 
   mapping = {
-    ['<C-b>'] = cmp.mapping(cmp.mapping.scroll_docs(-4), {'i', 'c'}),
-    ['<C-f>'] = cmp.mapping(cmp.mapping.scroll_docs(4), {'i', 'c'}),
-    ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), {'i', 'c'}),
-    ['<C-y>'] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-    ['<C-e>'] = cmp.mapping({i = cmp.mapping.abort(), c = cmp.mapping.close()}),
+    ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
+    ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
+    ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+    ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+    ["<C-e>"] = cmp.mapping { i = cmp.mapping.abort(), c = cmp.mapping.close() },
 
-    ['<CR>'] = cmp.mapping.confirm({select = false}), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    ["<CR>"] = cmp.mapping.confirm { select = false }, -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
     ["<Tab>"] = cmp.mapping(function(fallback)
       if luasnip.expand_or_jumpable() then
         luasnip.expand_or_jump()
       else
         fallback()
       end
-    end, {"i", "s"}),
+    end, { "i", "s" }),
 
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if luasnip.jumpable(-1) then
@@ -43,24 +41,24 @@ cmp.setup({
       else
         fallback()
       end
-    end, {"i", "s"})
+    end, { "i", "s" }),
   },
 
-  sources = cmp.config.sources(
-    {{name = 'buffer'}},
-    {
-      {name = 'nvim_lsp'}, {name = 'luasnip'}, -- For luasnip users.
-      {name = 'nvim_lua'}, {name = 'rg'}, {name = 'calc'}, {name = 'spell'},
-      {name = 'npm', keyword_length = 3}
-    }
-  )
-})
+  sources = cmp.config.sources({ { name = "buffer" } }, {
+    { name = "nvim_lsp" },
+    { name = "luasnip" }, -- For luasnip users.
+    { name = "nvim_lua" },
+    { name = "rg" },
+    { name = "calc" },
+    { name = "spell" },
+    { name = "npm", keyword_length = 3 },
+  }),
+}
 
 -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {sources = {{name = 'buffer'}}})
+cmp.setup.cmdline("/", { sources = { { name = "buffer" } } })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  sources = cmp.config.sources({{name = 'path'}}, {{name = 'cmdline'}})
+cmp.setup.cmdline(":", {
+  sources = cmp.config.sources({ { name = "path" } }, { { name = "cmdline" } }),
 })
-
