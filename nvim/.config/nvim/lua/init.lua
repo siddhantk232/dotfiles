5ad3ad9cc5ddb4004d3410f6d246b0d1786a53ae
siddhantCodes <siddhantk232@gmail.com>
2022-07-04 11:14:03 +0530

    nvim indent settings

--- nvim/.config/nvim/lua/init.lua
+++ nvim/.config/nvim/lua/init.lua
@@ -37,5 +37,7 @@ vim.opt.list = true
 vim.opt.listchars:append "eol:↴"
 
 require("indent_blankline").setup {
+  char = '┊',
+  show_trailing_blankline_indent = false,
   show_end_of_line = true,
 }
